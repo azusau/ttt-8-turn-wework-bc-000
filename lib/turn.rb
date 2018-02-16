4ad7d73cd447fb784fb0e3d591f0c5cbb91265e9
Azusa Uchikura <azusa.uchikura@wework.com>
2018-02-16 01:02:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   
-  if valid_move?(board, index,) == true
+  if valid_move?(board, index) == true
     move(board, index)
   else
    puts "Your move is invalid!"
